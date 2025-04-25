2858c97b1568647a388ac9b86c7d5495ebc4bbb9
jardimdanificado <jardeltorreslima01@gmail.com>
2025-04-25 01:56:55 -0300

    string package update

--- str/str.c
+++ str/str.c
@@ -2,50 +2,88 @@
 
 function(_str_concat)
 {
-    // pega ponteiros para as strings
     char *str1 = arg_s(0);
     char *str2 = arg_s(1);
 
-    // salva tamanhos
     int len1 = strlen(str1);
     int len2 = strlen(str2);
-
-    // salva a label da str2
-    Value label_str2 = (Value){.s = data_l(1)};
-
-    // cria novo bloco concatenado
+    
     int total_len = len1 + len2 + 1;
     Int blocks = (total_len + sizeof(void*) - 1) / sizeof(void*);
     Int new_index = new_block(vm, NULL, blocks);
 
-    // escreve str1 + str2 no novo bloco
-    memcpy(&vm->values->data[new_index].u8[0], str1, len1);
-    memcpy(&vm->values->data[new_index].u8[len1], str2, len2);
+    memmove(&vm->values->data[new_index].u8[0], str1, len1);
+    memmove(&vm->values->data[new_index].u8[len1], str2, len2);
     vm->values->data[new_index].u8[len1 + len2] = '\0';
 
-    // aplica a label da str1 ao novo bloco
     data_l(new_index) = arg_l(0);
-
-    // remove os blocos antigos da str1
-    for (Int i = 0; i < (len1 + 1 + sizeof(void*) - 1) / sizeof(void*); i++)
-    {
-        list_remove(vm->values, arg_i(0) + i);
-        list_remove(vm->labels, arg_i(0) + i);
-    }
-
-    // atualiza a referência da str1
-    arg_i(0) = new_index;
     arg_l(0) = NULL;
 
-    // restaura a label da str2
-    data_l(1) = label_str2.s;
+    Int str1_blocks = (strlen(str1) + 1 + sizeof(void*) - 1) / sizeof(void*);
+
+    for (Int i = 0; i < str1_blocks; i++)
+    {
+        list_remove(vm->values, arg_i(0));
+        list_remove(vm->labels, arg_i(0));
+    }
 
     return -1;
-
-
 }
+
+function(_str_set)
+{
+    char *str = arg_s(0);
+
+    Int position = arg(1).i;
+
+    char *content = arg_s(2);
+    Int content_len = strlen(content);
+
+    Int oldsize = strlen(str);
+
+    Int end_pos = position + content_len;
+
+    Int oldblocks = (oldsize + 1 + sizeof(void*) - 1) / sizeof(void*);
+    Int needed_blocks = (end_pos + 1 + sizeof(void*) - 1) / sizeof(void*);
+
+    if (needed_blocks <= oldblocks)
+    {
+        memmove(&str[position], content, content_len);
+    }
+    else
+    {
+        Int new_index = new_block(vm, NULL, needed_blocks);
+
+        memmove(&vm->values->data[new_index].u8[0], str, position);
+        memmove(&vm->values->data[new_index].u8[position], content, content_len);
+
+        if (oldsize > end_pos)
+        {
+            memmove(&vm->values->data[new_index].u8[end_pos], str + end_pos, oldsize - end_pos);
+        }
+
+        vm->values->data[new_index].u8[
+            (oldsize > end_pos ? oldsize : end_pos)
+        ] = '\0';
+
+        data_l(new_index) = arg_l(0);
+        arg_l(0) = NULL;
+
+        for (Int i = 0; i < oldblocks; i++)
+        {
+            list_remove(vm->values, arg_i(0));
+            list_remove(vm->labels, arg_i(0));
+        }
+
+        arg_i(0) = new_index;
+    }
+
+    return -1;
+}
+
 
 init(str)
 {
-    add_function(vm, "str.concat", _str_concat);
+    add_function(vm, "string.concat", _str_concat);
+    add_function(vm, "string.set", _str_set);
 }
