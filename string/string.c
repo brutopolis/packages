9f4e842bfb86da36ed939d3cd44d5bcd02733c3e
jardimdanificado <jardeltorreslima01@gmail.com>
2025-04-27 08:09:54 -0300

    minor change on string lib

--- string/string.c
+++ string/string.c
@@ -1,5 +1,19 @@
 #include "buxu.h"
 
+function(_str_concat)
+{
+    // concatenate two strings
+    char *str1 = arg_s(0);
+    char *str2 = arg_s(1);
+    char *result = malloc(strlen(str1) + strlen(str2) + 1);
+    strcpy(result, str1);
+    strcat(result, str2);
+    Int result_i = new_string(vm, NULL, result);
+    free(result);
+    return result_i;
+}
+
 init(string)
 {
+    add_function(vm, "string.concat", _str_concat);
 }
