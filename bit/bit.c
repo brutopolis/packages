740e9148269dedc91d88535376b71c26ac617bce
jardimdanificado <jardeltorreslima01@gmail.com>
2025-05-10 05:59:39 -0300

    new bit functions

--- bit/bit.c
+++ bit/bit.c
@@ -36,19 +36,30 @@ list_function(_bit_rshift)
     return -1;
 }
 
-list_function(_bit_set)
+list_function(_bit_on)
 {
     Int mask = 1 << arg(1).i;
     arg(0).i |= mask;
     return -1;
 }
 
-list_function(_bit_clear)
+list_function(_bit_off)
 {
     Int mask = ~(1 << arg(1).i);
     arg(0).i &= mask;
     return -1;
 }
+
+list_function(_bit_set) // set 0 or 1
+{
+    Int mask = 1 << arg(1).i;
+    if (arg(2).i)
+        arg(0).i |= mask;
+    else
+        arg(0).i &= ~mask;
+    return -1;
+}
+
 
 list_function(_bit_get)
 {
@@ -81,8 +92,9 @@ init(bit)
     add_function(context, "bit.not", _bit_not);
     add_function(context, "bit.lshift", _bit_lshift);
     add_function(context, "bit.rshift", _bit_rshift);
+    add_function(context, "bit.on", _bit_on);
+    add_function(context, "bit.off", _bit_off);
     add_function(context, "bit.set", _bit_set);
-    add_function(context, "bit.clear", _bit_clear);
     add_function(context, "bit.get", _bit_get);
     add_function(context, "bit.switch", _bit_switch);
     add_function(context, "bit.swap", _bit_swap);
