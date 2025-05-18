8af95e5db49972d90e4189b24ede521c88dee839
jardimdanificado <jardeltorreslima01@gmail.com>
2025-05-18 07:24:35 -0300

    minifb setup now does properly compile with -fPIC

--- minifb/setup.sh
+++ minifb/setup.sh
@@ -1,9 +1,12 @@
-git clone https://github.com/emoon/minifb.git;
-cd minifb;
-mkdir build;
-cd build;
-cmake .. -DUSE_WAYLAND_API=OFF -DUSE_OPENGL_API=ON;
-make;
-sudo make install;
-cd ..;
-cd ..;
+rm -rf minifb
+rm -rf lib
+git clone https://github.com/emoon/minifb
+mkdir -p lib include
+cd minifb
+mkdir -p build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DUSE_OPENGL_API=ON;
+make
+cd ..
+cp build/libminifb.a ../lib/
+cp include/*.h ../include/
