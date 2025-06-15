3f01f8544b94398918ffa9b6f2ad8ff8ee9d97b5
jardimdanificado <jardeltorreslima01@gmail.com>
2025-06-15 15:32:45 -0300

    math updated + new package: type

--- math/math.c
+++ math/math.c
@@ -1,5 +1,7 @@
 #include <bruter-representation.h>
 
+// int functions
+
 BR_FUNCTION(_iadd)
 {
     br_arg_set(context, args, 0, bruter_value_i(br_arg_get(context, args, 0).i + br_arg_get(context, args, 1).i));
@@ -40,6 +42,8 @@ BR_FUNCTION(_imod)
     return -1;
 }
 
+// float functions
+
 BR_FUNCTION(_fadd)
 {
     br_arg_set(context, args, 0, bruter_value_f(br_arg_get(context, args, 0).f + br_arg_get(context, args, 1).f));
@@ -75,17 +79,251 @@ BR_FUNCTION(_fmod)
     return -1;
 }
 
+// auto
+
+BR_FUNCTION(_aadd)
+{
+    switch (br_arg_get_type(context, args, 0))
+    {
+        case BR_TYPE_ANY:
+            switch (br_arg_get_type(context, args, 1))
+            {
+                case BR_TYPE_ANY:
+                    br_arg_set(context, args, 0, bruter_value_i(br_arg_get(context, args, 0).i + br_arg_get(context, args, 1).i));
+                    break;
+                case BR_TYPE_FLOAT:
+                    br_arg_set(context, args, 0, bruter_value_f(br_arg_get(context, args, 0).i + br_arg_get(context, args, 1).f));
+                    break;
+                default:
+                    printf("BR_ERROR: Unsupported type for addition\n");
+            }
+            break;
+        case BR_TYPE_FLOAT:
+            switch (br_arg_get_type(context, args, 1))
+            {
+                case BR_TYPE_ANY:
+                    br_arg_set(context, args, 0, bruter_value_f(br_arg_get(context, args, 0).f + br_arg_get(context, args, 1).i));
+                    break;
+                case BR_TYPE_FLOAT:
+                    br_arg_set(context, args, 0, bruter_value_f(br_arg_get(context, args, 0).f + br_arg_get(context, args, 1).f));
+                    break;
+                default:
+                    printf("BR_ERROR: Unsupported type for addition\n");
+            }
+            break;
+        default:
+            printf("BR_ERROR: autoadd could not determine type for operation\n");
+            return -1;
+    }
+    return -1;
+}
+
+BR_FUNCTION(_asub)
+{
+    switch (br_arg_get_type(context, args, 0))
+    {
+        case BR_TYPE_ANY:
+            switch (br_arg_get_type(context, args, 1))
+            {
+                case BR_TYPE_ANY:
+                    br_arg_set(context, args, 0, bruter_value_i(br_arg_get(context, args, 0).i - br_arg_get(context, args, 1).i));
+                    break;
+                case BR_TYPE_FLOAT:
+                    br_arg_set(context, args, 0, bruter_value_f(br_arg_get(context, args, 0).i - br_arg_get(context, args, 1).f));
+                    break;
+                default:
+                    printf("BR_ERROR: Unsupported type for subtraction\n");
+            }
+            break;
+        case BR_TYPE_FLOAT:
+            switch (br_arg_get_type(context, args, 1))
+            {
+                case BR_TYPE_ANY:
+                    br_arg_set(context, args, 0, bruter_value_f(br_arg_get(context, args, 0).f - br_arg_get(context, args, 1).i));
+                    break;
+                case BR_TYPE_FLOAT:
+                    br_arg_set(context, args, 0, bruter_value_f(br_arg_get(context, args, 0).f - br_arg_get(context, args, 1).f));
+                    break;
+                default:
+                    printf("BR_ERROR: Unsupported type for subtraction\n");
+            }
+            break;
+        default:
+            printf("BR_ERROR: autosub could not determine type for operation\n");
+            return -1;
+    }
+    return -1;
+}
+
+BR_FUNCTION(_amul)
+{
+    switch (br_arg_get_type(context, args, 0))
+    {
+        case BR_TYPE_ANY:
+            switch (br_arg_get_type(context, args, 1))
+            {
+                case BR_TYPE_ANY:
+                    br_arg_set(context, args, 0, bruter_value_i(br_arg_get(context, args, 0).i * br_arg_get(context, args, 1).i));
+                    break;
+                case BR_TYPE_FLOAT:
+                    br_arg_set(context, args, 0, bruter_value_f(br_arg_get(context, args, 0).i * br_arg_get(context, args, 1).f));
+                    break;
+                default:
+                    printf("BR_ERROR: Unsupported type for multiplication\n");
+            }
+            break;
+        case BR_TYPE_FLOAT:
+            switch (br_arg_get_type(context, args, 1))
+            {
+                case BR_TYPE_ANY:
+                    br_arg_set(context, args, 0, bruter_value_f(br_arg_get(context, args, 0).f * br_arg_get(context, args, 1).i));
+                    break;
+                case BR_TYPE_FLOAT:
+                    br_arg_set(context, args, 0, bruter_value_f(br_arg_get(context, args, 0).f * br_arg_get(context, args, 1).f));
+                    break;
+                default:
+                    printf("BR_ERROR: Unsupported type for multiplication\n");
+            }
+            break;
+        default:
+            printf("BR_ERROR: automul could not determine type for operation\n");
+            return -1;
+    }
+    return -1;
+}
+
+BR_FUNCTION(_adiv)
+{
+    switch (br_arg_get_type(context, args, 0))
+    {
+        case BR_TYPE_ANY:
+            switch (br_arg_get_type(context, args, 1))
+            {
+                case BR_TYPE_ANY:
+                    if (br_arg_get(context, args, 1).i == 0)
+                    {
+                        printf("BR_ERROR: Division by zero\n");
+                        return -1; // or handle error appropriately
+                    }
+                    br_arg_set(context, args, 0, bruter_value_i(br_arg_get(context, args, 0).i / br_arg_get(context, args, 1).i));
+                    break;
+                case BR_TYPE_FLOAT:
+                    if (br_arg_get(context, args, 1).f == 0.0f)
+                    {
+                        printf("BR_ERROR: Division by zero\n");
+                        return -1; // or handle error appropriately
+                    }
+                    br_arg_set(context, args, 0, bruter_value_f(br_arg_get(context, args, 0).i / br_arg_get(context, args, 1).f));
+                    break;
+                default:
+                    printf("BR_ERROR: Unsupported type for division\n");
+            }
+            break;
+        case BR_TYPE_FLOAT:
+            switch (br_arg_get_type(context, args, 1))
+            {
+                case BR_TYPE_ANY:
+                    if (br_arg_get(context, args, 1).i == 0)
+                    {
+                        printf("BR_ERROR: Division by zero\n");
+                        return -1; // or handle error appropriately
+                    }
+                    br_arg_set(context, args, 0, bruter_value_f(br_arg_get(context, args, 0).f / br_arg_get(context, args, 1).i));
+                    break;
+                case BR_TYPE_FLOAT:
+                    if (br_arg_get(context, args, 1).f == 0.0f)
+                    {
+                        printf("BR_ERROR: Division by zero\n");
+                        return -1; // or handle error appropriately
+                    }
+                    br_arg_set(context, args, 0, bruter_value_f(br_arg_get(context, args, 0).f / br_arg_get(context, args, 1).f));
+                    break;
+                default:
+                    printf("BR_ERROR: Unsupported type for division\n");
+            }
+            break;
+        default:
+            printf("BR_ERROR: autodiv could not determine type for operation\n");
+            return -1;
+    }
+    return -1;
+}
+
+BR_FUNCTION(_amod)
+{
+    switch (br_arg_get_type(context, args, 0))
+    {
+        case BR_TYPE_ANY:
+            switch (br_arg_get_type(context, args, 1))
+            {
+                case BR_TYPE_ANY:
+                    if (br_arg_get(context, args, 1).i == 0)
+                    {
+                        printf("BR_ERROR: Division by zero\n");
+                        return -1; // or handle error appropriately
+                    }
+                    br_arg_set(context, args, 0, bruter_value_i(br_arg_get(context, args, 0).i % br_arg_get(context, args, 1).i));
+                    break;
+                case BR_TYPE_FLOAT:
+                    if (br_arg_get(context, args, 1).f == 0.0f)
+                    {
+                        printf("BR_ERROR: Division by zero\n");
+                        return -1; // or handle error appropriately
+                    }
+                    br_arg_set(context, args, 0, bruter_value_f(fmod(br_arg_get(context, args, 0).i, br_arg_get(context, args, 1).f)));
+                    break;
+                default:
+                    printf("BR_ERROR: Unsupported type for modulo\n");
+            }
+            break;
+        case BR_TYPE_FLOAT:
+            switch (br_arg_get_type(context, args, 1))
+            {
+                case BR_TYPE_ANY:
+                    if (br_arg_get(context, args, 1).i == 0)
+                    {
+                        printf("BR_ERROR: Division by zero\n");
+                        return -1; // or handle error appropriately
+                    }
+                    br_arg_set(context, args, 0, bruter_value_f(fmod(br_arg_get(context, args, 0).f, br_arg_get(context, args, 1).i)));
+                    break;
+                case BR_TYPE_FLOAT:
+                    if (br_arg_get(context, args, 1).f == 0.0f)
+                    {
+                        printf("BR_ERROR: Division by zero\n");
+                        return -1; // or handle error appropriately
+                    }
+                    br_arg_set(context, args, 0, bruter_value_f(fmod(br_arg_get(context, args, 0).f, br_arg_get(context, args, 1).f)));
+                    break;
+                default:
+                    printf("BR_ERROR: Unsupported type for modulo\n");
+            }
+            break;
+        default:
+            printf("BR_ERROR: automod could not determine type for operation\n");
+            return -1;
+    }
+    return -1;
+}
+
 BR_INIT(math)
 {
+    // int
     br_add_function(context, "i+", _iadd);
     br_add_function(context, "i-", _isub);
     br_add_function(context, "i*", _imul);
     br_add_function(context, "i/", _idiv);
     br_add_function(context, "i%", _imod);
-
+    // float
     br_add_function(context, "f+", _fadd);
     br_add_function(context, "f-", _fsub);
     br_add_function(context, "f*", _fmul);
     br_add_function(context, "f/", _fdiv);
     br_add_function(context, "f%", _fmod);
+    // auto 
+    br_add_function(context, "a+", _aadd);
+    br_add_function(context, "a-", _asub);
+    br_add_function(context, "a*", _amul);
+    br_add_function(context, "a/", _adiv);
+    br_add_function(context, "a%", _amod);
 }
--- math/package.sh
+++ math/package.sh
@@ -1,5 +1,5 @@
 name math
-version 0.0.8
+version 0.0.9
 description "buxu math library"
 files math.c
 setup "bucc -o math.brl math.c -lm"
